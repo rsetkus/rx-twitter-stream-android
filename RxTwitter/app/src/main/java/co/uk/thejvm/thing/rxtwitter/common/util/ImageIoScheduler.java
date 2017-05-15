bde8c1705a9737f4ed7f12cb81c668e6de9c1b60
James <james.pittendreigh@ee.co.uk>
2017-05-15 18:42:01 +0100

    Adding new IO scheduler for image retrieval

--- RxTwitter/app/src/main/java/co/uk/thejvm/thing/rxtwitter/common/util/ImageIoScheduler.java
+++ RxTwitter/app/src/main/java/co/uk/thejvm/thing/rxtwitter/common/util/ImageIoScheduler.java
@@ -3,9 +3,9 @@ package co.uk.thejvm.thing.rxtwitter.common.util;
 import io.reactivex.Scheduler;
 import io.reactivex.schedulers.Schedulers;
 
-public class IOScheduler implements ExecutionScheduler{
+public class ImageIoScheduler implements ExecutionScheduler {
     @Override
     public Scheduler getScheduler() {
-        return Schedulers.io();
+        return Schedulers.computation();
     }
 }
